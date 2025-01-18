5a81bc19c6ff50e830dd99736e9475554d4befd8
Toran <Theo2000@gmx.de>
2025-01-19 00:14:47 +0100

    No big changes

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         LOGGER.load("C:\\Users\\Toran\\IdeaProjects\\MandelbrotmengenGenerator\\res\\logs\\log.txt");
         Mengenanzeige2 ma = new Mengenanzeige2(2,new Complex(-0.8, 0.156),false);
 
+
         //ma.close();
         //Mengenanzeige ma2 = new Mengenanzeige(2,new Complex(-0.8, 0.156),true,-1.75,1.75,1.0-1,-1.0-1);
     }
